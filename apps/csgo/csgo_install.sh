2adc56d217a69c36b66e7c7847943e99345abb99
Fred Damstra <fred.damstra@gmail.com>
2018-05-01 07:18:17 -0400

    CSGO Config Added

--- apps/csgo/csgo_install.sh
+++ apps/csgo/csgo_install.sh
@@ -5,5 +5,6 @@
 source /root/credentials.sh
 
 cd /root/steam
-./steamcmd.sh +login ${STEAM_USERNAME} ${STEAM_PASSWORD} +app_update 730 validate +quit
+./steamcmd.sh +login ${STEAM_USERNAME} ${STEAM_PASSWORD} +app_update 740 validate +quit
 
+cp autoexec.cfg server.cfg "/root/steam/steamapps//common/Counter-Strike\ Global\ Offensive\ Beta\ -\ Dedicated\ Server/csgo/"
--- apps/csgo/csgo_start.sh
+++ apps/csgo/csgo_start.sh
@@ -1,4 +1,17 @@
 #! /bin/bash
-cd /root/steam/steamapps/common/chivalry_ded_server/Binaries/Linux 
-./UDKGameServer-Linux AOCCTF-Moor_p?steamsockets -Port=7000 -QueryPort=7500 -seekfreeloadingserver
+cd "/root/steam/steamapps/common/Counter-Strike\ Global\ Offensive\ Beta\ -\ Dedicated\ Server/"
 
+# Classic Casual
+#srcds_run -game csgo -console -usercon +game_type 0 +game_mode 0 +mapgroup mg_active +map de_dust2
+
+# Classic Competitive
+#srcds_run -game csgo -console -usercon +game_type 0 +game_mode 1 +mapgroup mg_active +map de_dust2
+
+# Arms Race
+#srcds_run -game csgo -console -usercon +game_type 1 +game_mode 0 +mapgroup mg_armsrace +map ar_shoots
+
+# Demolition
+#srcds_run -game csgo -console -usercon +game_type 1 +game_mode 1 +mapgroup mg_demolition +map de_lake
+
+# Deathmatch
+./srcds_run -game csgo -console -usercon +game_type 1 +game_mode 2 +mapgroup mg_allclassic +map de_dust +sv_setsteamaccount ${CSGO_KEY} -net_port_try 1
