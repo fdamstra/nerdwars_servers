44ad99785b7d4b6693fbe72aa49fe57d8be166da
Gogs <gogs@fake.local>
2019-01-18 14:30:48 -0500

    Removed overload

--- apps/overload/overload_install.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-source /root/credentials.sh
-
-cd /root/steam
-./steamcmd.sh +login ${STEAM_USERNAME} ${STEAM_PASSWORD} +force_install_dir /root/overload +app_update 448850 validate +quit
-
-#cp /root/nerdwars_servers/apps/csgo/{autoexec.cfg,server.cfg} /root/csgo/csgo/cfg
